6062e3c799326530b180d726a709197772b1ffdb
23pa1a45a7-svg <23pa1a45a7@vishnu.edu.in>
2026-05-05 21:12:01 +0530

    Create queries.sql

---
+++ week1/day1/queries.sql
@@ -0,0 +1,140 @@
+-- 1
+SELECT * FROM Employee;
+
+-- 2
+SELECT name, salary FROM Employee;
+
+-- 3
+SELECT * FROM Employee WHERE age > 30;
+
+-- 4
+SELECT name FROM Department;
+
+-- 5
+SELECT e.*
+FROM Employee e
+JOIN Department d ON e.department_id = d.department_id
+WHERE d.name = 'IT';
+-- 6
+SELECT * FROM Employee WHERE name LIKE 'J%';
+
+-- 7
+SELECT * FROM Employee WHERE name LIKE '%e';
+
+-- 8
+SELECT * FROM Employee WHERE name LIKE '%a%';
+
+-- 9
+SELECT * FROM Employee WHERE LENGTH(name) = 9;
+
+-- 10
+SELECT * FROM Employee WHERE name LIKE '_o%';
+-- 11
+SELECT * FROM Employee WHERE YEAR(hire_date) = 2020;
+
+-- 12
+SELECT * FROM Employee WHERE MONTH(hire_date) = 1;
+
+-- 13
+SELECT * FROM Employee WHERE hire_date < '2019-01-01';
+
+-- 14
+SELECT * FROM Employee WHERE hire_date >= '2021-03-01';
+
+-- 15
+SELECT * FROM Employee
+WHERE hire_date >= CURDATE() - INTERVAL 2 YEAR;
+-- 16
+SELECT SUM(salary) FROM Employee;
+
+-- 17
+SELECT AVG(salary) FROM Employee;
+
+-- 18
+SELECT MIN(salary) FROM Employee;
+
+-- 19
+SELECT department_id, COUNT(*) 
+FROM Employee
+GROUP BY department_id;
+
+-- 20
+SELECT department_id, AVG(salary)
+FROM Employee
+GROUP BY department_id;
+-- 21
+SELECT department_id, SUM(salary)
+FROM Employee
+GROUP BY department_id;
+
+-- 22
+SELECT department_id, AVG(age)
+FROM Employee
+GROUP BY department_id;
+
+-- 23
+SELECT YEAR(hire_date), COUNT(*)
+FROM Employee
+GROUP BY YEAR(hire_date);
+
+-- 24
+SELECT department_id, MAX(salary)
+FROM Employee
+GROUP BY department_id;
+
+-- 25
+SELECT d.name
+FROM Employee e
+JOIN Department d ON e.department_id = d.department_id
+GROUP BY d.name
+ORDER BY AVG(e.salary) DESC
+LIMIT 1;
+-- 26
+SELECT department_id, COUNT(*)
+FROM Employee
+GROUP BY department_id
+HAVING COUNT(*) > 2;
+
+-- 27
+SELECT department_id, AVG(salary)
+FROM Employee
+GROUP BY department_id
+HAVING AVG(salary) > 55000;
+
+-- 28
+SELECT YEAR(hire_date), COUNT(*)
+FROM Employee
+GROUP BY YEAR(hire_date)
+HAVING COUNT(*) > 1;
+
+-- 29
+SELECT department_id, SUM(salary)
+FROM Employee
+GROUP BY department_id
+HAVING SUM(salary) < 100000;
+
+-- 30
+SELECT department_id, MAX(salary)
+FROM Employee
+GROUP BY department_id
+HAVING MAX(salary) > 75000;
+-- 31
+SELECT * FROM Employee ORDER BY salary ASC;
+
+-- 32
+SELECT * FROM Employee ORDER BY age DESC;
+
+-- 33
+SELECT * FROM Employee ORDER BY hire_date ASC;
+
+-- 34
+SELECT * FROM Employee ORDER BY department_id, salary;
+
+-- 35
+SELECT d.name, SUM(e.salary) AS total_salary
+FROM Department d
+JOIN Employee e ON d.department_id = e.department_id
+GROUP BY d.name
+ORDER BY total_salary;
+only i did these above ones
+tell me how to upload in git very clearly
